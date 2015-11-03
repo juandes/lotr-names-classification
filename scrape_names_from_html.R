f0ead2cba30ce52620bb29bdff71116e44e4c0e3
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2015-11-02 22:50:40 -0400

    For loop to extract the information

--- scrape_names_from_html.R
+++ scrape_names_from_html.R
@@ -1,11 +1,20 @@
 library(rvest)
-lotr_project <- read_html("C:/Users/Juan De Dios Santos/Desktop/LOTR_html.txt") 
-lotr_project%>% 
-  html_nodes("#481") %>%
-  html_text()
+html_data <- read_html("~/Development/lotr-names-classification/lotr-names-html.html")
+characters_data <- data.frame(name = character(0), race = character(0),
+                              stringsAsFactors = FALSE)
 
-lotr_project %>% 
-  html_nodes("#481") %>%
-  html_attr("class")
-
-# Last character is 952
+for (i in 1:952){
+  
+  # Get the name
+  name <- html_data %>%
+    html_nodes(paste0('#', i)) %>%
+    html_text()
+  
+  race_and_class <- strsplit(html_data %>%
+    html_nodes(paste0('#', i)) %>%
+    html_attr('class'), split = ' ')
+  
+  if (length(name) > 0) {
+    characters_data[i,] <- list(name, race_and_class[[1]][length(race_and_class[[1]]) - 1])
+  }
+}
