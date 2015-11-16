c67719037700e226993bc2de53cd6c7b29b5dc8d
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2015-11-16 23:59:41 +0100

    Cleaned data and creation of two new data files

--- scrape_names_from_html.R
+++ scrape_names_from_html.R
@@ -18,3 +18,34 @@ for (i in 1:952){
     characters_data[i,] <- list(name, race_and_class[[1]][length(race_and_class[[1]]) - 1])
   }
 }
+
+# Remove rows with NA
+characters_data <- na.omit(characters_data)
+# Remove rows where name is '?'
+characters_data <- characters_data[grep('\\?', characters_data$name, invert = TRUE), ]
+# Remove \n from the names
+characters_data$name <- sub('\n', '', characters_data$name)
+# Remove the prefix '1st', '2nd', etc.
+characters_data$name <- sub('[0-9]?[0-9][a-z]{2}', '', characters_data$name)
+table(characters_data$race)
+# Subset the races that have a significant number of entries
+characters_data <- characters_data[characters_data$race == 'Ainur' | 
+                        characters_data$race == 'Dwarf' |
+                        characters_data$race == 'Elf' |
+                        characters_data$race == 'Half-elf' |
+                        characters_data$race == 'Hobbit' |
+                        characters_data$race == 'Man', ]
+# Change the half-elves for elves (sorry Elrond)
+characters_data$race[characters_data$race == 'Half-elf'] <- 'Elf'
+# Remove trailing spaces
+characters_data$name <- sub('[ \t]+$', '', characters_data$name)
+# Remove an entry where the name is 'Others'
+characters_data <- characters_data[characters_data$name != 'Others' & 
+                                     characters_data$name != 'Master of La...', ]
+
+# This names of the characters on this dataframe won't have any surnames or
+# numbers on their name; we'll keep just the first name.
+characters_no_surnames <- characters_data
+
+# Regex to remove everything after the first whitespace
+characters_no_surnames$name <- sub(' .*', '', characters_no_surnames$name)
